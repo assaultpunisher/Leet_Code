ac0451ceea7d454e12a46208443fb2eea9d84197
Yugam <63348402+yugam08@users.noreply.github.com>
2021-04-28 23:44:44 +0530

    Code

---
+++ Medium/Python 3/Coin_Change_2(518).py
@@ -0,0 +1,11 @@
+class Solution:
+    def change(self, amount, coins):
+        # write your code here
+        dp = [0]*(amount+1)
+        dp[0]=1
+        
+        for coin in coins:
+            for x in range(coin, amount+1):
+                dp[x] += dp[x-coin]
+        return dp[amount]
+        
